730a99db1cc931e0b8f15f3baa6bafdb8e70f827
shaunbseidman <31448025+shaunbseidman@users.noreply.github.com>
2018-04-11 22:51:54 -0600

    changed verbage

--- src/components/who.vue
+++ src/components/who.vue
@@ -4,7 +4,7 @@
   <div id="who">
     <div class="animated fadeIn" id="aboutMe">
       <p>Thanks for taking a look. I am a full stack developer with a passion for creating, problem solving and continuing to learn and grow. My previous experience in managing a small business and team building have helped to solidify my work techniques
-        with others as well as instill a never-quit attitude. <br><br>I would love to establish myself in the web development world and believe my skills as well as willingness to learn would make me a great candidate for a junior dev position. <br><br>Please
+        with others and also instill a never-quit attitude. <br><br>I would love to establish myself in the web development world and believe my skills, as well as my willingness to learn, would make me a great candidate for a junior dev position. <br><br>Please
         feel free to take a look at my resume below.<br> I look forward to talking and hopefully working together.
       </p><br>
       <a href="https://github.com/shaunbseidman/resume/blob/master/res.pdf"><img class = 'linkImages' id='img2' src="../assets/res.png" alt="git"></a>
