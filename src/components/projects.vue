a012e627d4933b203f7dc34fd29ae6e4eb4588fc
shaunbseidman <31448025+shaunbseidman@users.noreply.github.com>
2018-04-13 13:58:22 -0600

    more shit

--- src/components/projects.vue
+++ src/components/projects.vue
@@ -4,7 +4,6 @@
 
   <div class="animated fadeIn" id="projectShit">
     <div class="shoeGif">
-
       <h3>What's it Worth</h3>
       <img class='gif' src="../assets/gif2.gif" alt="">
       <div class="tech">
